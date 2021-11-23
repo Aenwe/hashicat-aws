2e2878443d14b7a486134d2bf288b09928c6a95f
fer bogomolny <fmbogomolny@gmail.com>
2021-11-23 23:21:49 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=3.42.0"
+    }
+  }
+}
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
